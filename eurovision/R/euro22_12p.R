430ba8c88ac0efadd8db66ae01c943a61c2e5844
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-05-20 13:06:47 +0100

    Add eurovision analysis

---
+++ eurovision/R/euro22_12p.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(ggalluvial)
+library(showtext)
+
+# Init theme --------------------------------------------------------------
+showtext_auto()
+font_add_google('Bitter')
+theme_set(theme_minimal(base_family = 'Bitter'))
+
+# Load data ---------------------------------------------------------------
+df <- read_csv(here::here('eurovision/data/eurovision-votes.csv')) %>%
+  filter(
+    year == 2022,
+    points == 12,
+    jury_or_televoting == 'J',
+    semi_final == 'f'
+  )
+
+# Plot data ---------------------------------------------------------------
+df %>%
+  ggplot(aes(axis1 = from_country, axis2 = to_country, y = points)) +
+    geom_alluvium(aes(fill = to_country), alpha = 0.75) +
+    geom_stratum(aes(fill = to_country, color = to_country)) +
+    geom_text(stat = 'stratum', aes(label = after_stat(stratum), family = 'Bitter'), color = 'gray15') +
+    scale_x_discrete(limits = c('From', 'To'), expand = c(.1, .1)) +
+    labs(
+      x = NULL, y = NULL,
+      title = 'Our 12 points go to...',
+      subtitle = '2022 Eurovision song contest. Countries giving 12 points (Final | Jury voting)',
+      caption = 'Visualization by @sdelquin  |  Data: TidyTuesday (R for Data Science)'
+    ) +
+    theme(
+      legend.position = 'none',
+      panel.grid = element_blank(),
+      plot.title = element_text(size = 26, hjust = 0, face = 'bold'),
+      plot.subtitle = element_text(size = 14, hjust = 0, color = 'gray50', face = 'bold'),
+      plot.caption = element_text(size = 10, color = 'gray40'),
+      axis.text.x = element_text(face = 'bold', size = 14, vjust = 4, color = 'gray30'),
+      axis.text.y = element_blank()
+    )
+  
+ggsave(here::here('eurovision/plots/euro22_12p.pdf'), width = 8, height = 9, device = cairo_pdf)
+ggsave(here::here('eurovision/plots/euro22_12p.jpg'), width = 8, height = 9)
